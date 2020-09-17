9c7f3170747bc1efbac606d47997ce44579ce3c0
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-09-17 16:43:06 +0200

    Updates

---
+++ precache-manifest.8464227964aa7426d79468e155730fa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a773c874b5a4e42f1b9529ae966b92",
+    "url": "/to-do-list-react-redux/index.html"
+  },
+  {
+    "revision": "d5cb3b95f029ec5e7924",
+    "url": "/to-do-list-react-redux/static/js/2.92ba89b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do-list-react-redux/static/js/2.92ba89b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086b7340712206bb6aab",
+    "url": "/to-do-list-react-redux/static/js/main.a472c1f2.chunk.js"
+  },
+  {
+    "revision": "2ea66278fad76940f275",
+    "url": "/to-do-list-react-redux/static/js/runtime-main.29a88414.js"
+  }
+]);
