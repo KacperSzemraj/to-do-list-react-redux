bad588b2a1e91aa9e9dcd04eddb1ac0233afd497
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-09-18 23:22:54 +0200

    App changed to Tasks

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -7,7 +7,7 @@ import Header from "./Header";
 import Container from "./Container";
 import { useTasks } from "../../useTasks";
 
-function App() {
+function Tasks() {
   const [hideDone, setHideDone] = useState(false);
 
   const toggleHideDone = () => {
@@ -53,4 +53,4 @@ function App() {
   );
 }
 
-export default App;
+export default Tasks;
