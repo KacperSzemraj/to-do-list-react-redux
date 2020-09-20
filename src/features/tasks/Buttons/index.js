80eb7227e37a4fe6df400b2c3ec841671556ada4
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-09-20 17:36:12 +0200

    toggleHideDone reducer added

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,22 +1,28 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectTasks, toggleHideDone } from "../tasksSlice";
 import { ButtonsWrapper, Button } from "./styled";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-    <ButtonsWrapper>
-        {tasks.length > 0 && (
-            <>
-                <Button onClick={toggleHideDone}>
-                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
-			    </Button>
-                <Button
-                    onClick={setAllDone}
-                    disabled={tasks.every(({ done }) => done)}
-                >
-                    Ukończ wszystkie
-			    </Button>
-            </>
-        )}
-    </ButtonsWrapper>
-);
+const Buttons = ({ setAllDone }) => {
+    const { tasks, hideDone } = useSelector(selectTasks);
+    const dispatch = useDispatch();
+    return (
+        <ButtonsWrapper>
+            {tasks.length > 0 && (
+                <>
+                    <Button onClick={() => dispatch(toggleHideDone())}>
+                        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                    </Button>
+                    <Button
+                        onClick={setAllDone}
+                        disabled={tasks.every(({ done }) => done)}
+                    >
+                        Ukończ wszystkie
+                    </Button>
+                </>
+            )}
+        </ButtonsWrapper>
+    )
+};
 
 export default Buttons;
--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import Form from "./Form";
 import TaskList from "./TaskList";
@@ -11,12 +11,6 @@ import { selectTasks } from './tasksSlice';
 
 
 function Tasks() {
-  const [hideDone, setHideDone] = useState(false);
-
-  const toggleHideDone = () => {
-    setHideDone(hideDone => !hideDone);
-  };
-
   const { tasks } = useSelector(selectTasks);
 
   const {
@@ -40,15 +34,12 @@ function Tasks() {
         body={
           <TaskList
             tasks={tasks}
-            hideDone={hideDone}
             removeTask={removeTask}
             toggleTaskDone={toggleTaskDone} />
         }
         extraHeaderContent={
           <Buttons
             tasks={tasks}
-            hideDone={hideDone}
-            toggleHideDone={toggleHideDone}
             setAllDone={setAllDone}
           />
         }
