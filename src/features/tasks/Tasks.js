32bc9795610ce9aa191f1a70e88c93b40b273831
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-09-20 21:50:18 +0200

    code refactored

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -5,31 +5,21 @@ import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
-import { useTasks } from "../../useTasks";
 
 function Tasks() {
-
-  const {
-    addNewTask,
-  } = useTasks();
 
   return (
     <Container className="container">
       <Header title="Lista zadań" />
-
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
+        body={<Form />}
       />
 
       <Section
         title="Lista zadań"
-        body={
-          <TaskList />
-        }
-        extraHeaderContent={
-          <Buttons />
-        }
+        body={<TaskList />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
